05f5c2a045cbd64a3617c3fd3f34248ba8a4f321
azizdevfull <aziz16110904@gmail.com>
2022-11-11 18:04:40 +0500

    completed random with function to user_id

--- signup.php
+++ signup.php
@@ -2,8 +2,34 @@
 
 session_start();
 
-        include("connect.php");
+        include("connection.php");
         include("functions.php");
+
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST')
+        {
+        // for post requests
+        $user_name = $_POST['user_name'];
+        $password = $_POST['password'];
+
+        if (!empty($user_name) && !empty($password) && !is_numeric($user_name)) {
+            
+            //save to db
+            $user_id = random_num(20);
+            $query = "insert into users (user_id, user_name, password) values ('$user_id', '$user_name', '$password')";
+           
+            mysqli_query($query);
+
+            header("Location: login.php");
+            
+            die;
+        }
+        else
+        {
+            echo "Please enter your username and password!";
+        }
+
+        }
         
 ?>
 
