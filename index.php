f2f444dc36293c60b224713ae55894662ef66ba2
azizdevfull <aziz16110904@gmail.com>
2022-11-11 17:04:36 +0500

    added all necessary files

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Auth</title>
+</head>
+<body>
+    <a href="logout.php">Log Out</a>
+    <h1>This is index page</h1>
+    <br>
+    Hello User
+</body>
+</html>
